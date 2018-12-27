c1f9b13f4a023ac1024a8d220e1fcce57524b937
大沼 峻徳 <ohnhistory@gmail.com>
2018-12-27 18:18:04 +0900

    名前の変更

---
+++ src/javascripts/API.js
@@ -0,0 +1,13 @@
+import CancelableAPI from '../../lib/CancelableAPI';
+
+class API extends CancelableAPI {
+  fetch() {
+    console.log('request');
+    return this.request({
+      method: CancelableAPI.GET,
+      endpoint: '/data'
+    });
+  }
+}
+
+export default new API();
