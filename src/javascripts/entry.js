cc5b59a3a5acc87d969f6a6175952bac025e6971
大沼 峻徳 <ohnhistory@gmail.com>
2018-12-27 19:07:56 +0900

    サンプルコードを書く

--- src/javascripts/entry.js
+++ src/javascripts/entry.js
@@ -1,4 +1,45 @@
-import API from './API';
+import { API_ROOT } from './constants/API';
 
+import BaseAPI from './BaseAPI';
+
+const API = new BaseAPI(API_ROOT);
+const promiseList1 = [];
+
+function updateStatus() {
+  status.innerText = promiseList1.map((status) => status.status).join(', ');
+}
+
+const buttonElement = document.getElementById('button-request-1');
+const status = document.getElementById('status-1');
+const cancelButtonElement = document.getElementById('button-cancel-all-1');
+console.log(status);
+buttonElement.addEventListener('click', () => {
+  const promise = API.fetch();
+
+  // ステータスをログとして持っておく
+  const statusObj = {
+    status: 'pending'
+  };
+  promiseList1.push(statusObj);
+  promise
+    .then(() => { statusObj.status = 'success'; })
+    .catch(({ isCancel }) => { statusObj.status = isCancel ? 'cancel' : 'error'; });
+  console.log(promiseList1);
+
+  updateStatus();
+  promise
+    .then((response) => {
+      console.log(response);
+    })
+    .catch(({ isCancel, err }) => {
+      console.log(isCancel, err);
+    })
+    .finally(() => {
+      updateStatus();
+    });
+});
+
+cancelButtonElement.addEventListener('click', () => {
+  API.cancelAll();
+});
 console.log(API);
-API.fetch();
