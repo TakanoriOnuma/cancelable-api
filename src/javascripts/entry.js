025073209feaa52ec6b66b4038c1be7df330d09f
大沼 峻徳 <ohnhistory@gmail.com>
2019-01-05 16:13:02 +0900

    API1とAPI2でそれぞれキャンセルするテストコードを書いた

--- src/javascripts/entry.js
+++ src/javascripts/entry.js
@@ -2,44 +2,83 @@ import { API_ROOT } from './constants/API';
 
 import BaseAPI from './BaseAPI';
 
-const API = new BaseAPI(API_ROOT);
-const promiseList1 = [];
+// API1の設定
+{
+  const API = new BaseAPI(API_ROOT);
+  const promiseList = [];
 
-function updateStatus() {
-  status.innerText = promiseList1.map((status) => status.status).join(', ');
+  const statusElement = document.getElementById('status-1');
+  const updateStatus = () => {
+    statusElement.innerText = promiseList.map((status) => status.status).join(', ');
+  };
+
+  const buttonElement = document.getElementById('button-request-1');
+  buttonElement.addEventListener('click', () => {
+    const promise = API.fetch();
+
+    // ステータスをログとして持っておく
+    const statusObj = {
+      status: 'pending'
+    };
+    promiseList.push(statusObj);
+    promise
+      .then(() => { statusObj.status = 'success'; })
+      .catch(({ isCancel }) => { statusObj.status = isCancel ? 'cancel' : 'error'; });
+
+    updateStatus();
+    promise
+      .then(() => {})
+      .catch(() => {})
+      .finally(() => {
+        updateStatus();
+      });
+  });
+
+  const cancelButtonElement = document.getElementById('button-cancel-all-1');
+  cancelButtonElement.addEventListener('click', () => {
+    API.cancelAll();
+  });
 }
 
-const buttonElement = document.getElementById('button-request-1');
-const status = document.getElementById('status-1');
-const cancelButtonElement = document.getElementById('button-cancel-all-1');
-console.log(status);
-buttonElement.addEventListener('click', () => {
-  const promise = API.fetch();
+// API2の設定
+{
+  const API = new BaseAPI(API_ROOT);
+  const promiseList = [];
 
-  // ステータスをログとして持っておく
-  const statusObj = {
-    status: 'pending'
+  const statusElement = document.getElementById('status-2');
+  const updateStatus = () => {
+    statusElement.innerText = promiseList.map((status) => status.status).join(', ');
   };
-  promiseList1.push(statusObj);
-  promise
-    .then(() => { statusObj.status = 'success'; })
-    .catch(({ isCancel }) => { statusObj.status = isCancel ? 'cancel' : 'error'; });
-  console.log(promiseList1);
 
-  updateStatus();
-  promise
-    .then((response) => {
-      console.log(response);
-    })
-    .catch(({ isCancel, err }) => {
-      console.log(isCancel, err);
-    })
-    .finally(() => {
-      updateStatus();
-    });
-});
+  const buttonElement = document.getElementById('button-request-2');
+  buttonElement.addEventListener('click', () => {
+    const promise = API.fetch();
 
-cancelButtonElement.addEventListener('click', () => {
-  API.cancelAll();
+    // ステータスをログとして持っておく
+    const statusObj = {
+      status: 'pending'
+    };
+    promiseList.push(statusObj);
+    promise
+      .then(() => { statusObj.status = 'success'; })
+      .catch(({ isCancel }) => { statusObj.status = isCancel ? 'cancel' : 'error'; });
+
+    updateStatus();
+    promise
+      .then(() => {})
+      .catch(() => {})
+      .finally(() => {
+        updateStatus();
+      });
+  });
+
+  const cancelButtonElement = document.getElementById('button-cancel-all-2');
+  cancelButtonElement.addEventListener('click', () => {
+    API.cancelAll();
+  });
+}
+
+// 全てのAPIをキャンセルする
+document.getElementById('button-cancel-all').addEventListener('click', () => {
+  BaseAPI.cancelAll();
 });
-console.log(API);
